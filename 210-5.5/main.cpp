6c669615ab98d1c149a04c9e58f2bf749ee04e54
cwlucas41 <cwlucas41@gmail.com>
2015-03-21 16:11:56 -0500

    simple test main

--- 210-5.5/main.cpp
+++ 210-5.5/main.cpp
@@ -15,11 +15,14 @@ using namespace std;
 int main(int argc, const char * argv[]) {
 	Object* h = new Object [5];
 	PriorityQueue myPQ = PriorityQueue(h, 0, 5);
-	myPQ.enqueue(Object(2, 2));
-	myPQ.enqueue(Object(4,4));
-	myPQ.changeWeight(2, 5);
-	cout << myPQ.dequeue().getID() << endl;
-	cout << myPQ.dequeue().getID() << endl;
+	myPQ.enqueue(Object(1, 2));
+	myPQ.enqueue(Object(2, 4));
+	myPQ.enqueue(Object(3, 5));
+	myPQ.changeWeight(1, 6);
+	for (int i = 0; i < 3; i++) {
+		cout << myPQ.dequeue().getID() << endl;
+	}
+	
 	delete [] h;
     return 0;
 }
